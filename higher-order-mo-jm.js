9c9c225d23cc670c6ba7ee4fd2ec5cb23b6b760e
Jerrod <chaddjed@gmail.com>
2023-10-10 11:14:06 -0700

    add challenger higher order

---
+++ higher-order-mo-jm.js
@@ -0,0 +1,21 @@
+// Copy the challenges into your JavaScript file. Comment out the instructions and code the solution to each problem beneath the prompt.
+
+// Create a function that takes in an array of numbers and returns an array with all numbers multiplied by 10.
+// const arr1 = [3, 9, 15, 4, 10]
+// // output: [30, 90, 150, 40, 100]
+const arr1 = [3, 9, 15, 4, 10]
+const mult2 = (array) => {
+    return arr1.map ((value) => value * 10)
+}
+console.log (mult2(arr1))
+// input = arr1
+// output: [30, 90, 150, 40, 100]
+// declared funtcion .map, mult2, value multiplie by 10, 
+
+
+// Create a function that takes in an array of numbers and returns an array with all numbers divided by two.
+// const arr1 = [3, 9, 15, 4, 10]
+// // output: [1.5, 4.5, 7.5, 2, 5]
+// Create a function that takes in an array of numbers and returns an array with only odd numbers.
+// const arr2 = [2, 7, 3, 5, 8, 10, 13, -9]
+// // output: [7, 3, 5, 13, -9]
