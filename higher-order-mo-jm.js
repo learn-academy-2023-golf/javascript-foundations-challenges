ec181290b92d3ef39613b4a9270c02644cd9b4db
Markayel <mtorelus@gmail.com>
2023-10-10 13:30:32 -0700

    Completed more problems

--- higher-order-mo-jm.js
+++ higher-order-mo-jm.js
@@ -3,19 +3,31 @@
 // Create a function that takes in an array of numbers and returns an array with all numbers multiplied by 10.
 // const arr1 = [3, 9, 15, 4, 10]
 // // output: [30, 90, 150, 40, 100]
-const arr1 = [3, 9, 15, 4, 10]
-const mult2 = (array) => {
-    return arr1.map ((value) => value * 10)
-}
-console.log (mult2(arr1))
+// const arr1 = [3, 9, 15, 4, 10]
+// const mult2 = (array) => {
+//     return arr1.map ((value) => value * 10)
+// }
+// console.log (mult2(arr1))
 // input = arr1
 // output: [30, 90, 150, 40, 100]
 // declared funtcion .map, mult2, value multiplie by 10, 
 
 
-// Create a function that takes in an array of numbers and returns an array with all numbers divided by two.
-// const arr1 = [3, 9, 15, 4, 10]
+//Create a function that takes in an array of numbers and returns an array with all numbers divided by two.
+const arr1 = [3, 9, 15, 4, 10]
 // // output: [1.5, 4.5, 7.5, 2, 5]
+
+const div1 = (array) => {
+    return array.map((value) => value / 2)
+}
+console.log (div1(arr1))
+// 
+
 // Create a function that takes in an array of numbers and returns an array with only odd numbers.
 // const arr2 = [2, 7, 3, 5, 8, 10, 13, -9]
-// // output: [7, 3, 5, 13, -9]
+// // // output: [7, 3, 5, 13, -9]
+
+// const getOnlyOdd = (array) => {
+//     return array.filter((value) => value %2 !==0)
+// }
+// console.log(getOnlyOdd(arr2))
