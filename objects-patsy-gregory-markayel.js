238583ba4b0bb1a6ca0f336738a14b45af85791e
Greg <gvasquezencalada@gmail.com>
2023-10-12 13:22:04 -0700

    switched drivers- continued challenge work

--- objects-patsy-gregory-markayel.js
+++ objects-patsy-gregory-markayel.js
@@ -47,24 +47,48 @@ console.log(totalWithTax(product))
 
 // Consider this variable:
 
-// const lunch = {
-//   name: "PB and Banana",
-//   type: "sandwich",
-//   ingredients: ["bread", "peanut butter", "banana"]
-// }
+const lunch = {
+  name: "PB and Banana",
+  type: "sandwich",
+  ingredients: ["bread", "peanut butter", "banana"]
+}
 // Write the code that accesses the ingredients property.
+console.log(lunch.ingredients)
+
+
 // Write the code that access the third ingredient of the lunch object.
+console.log(lunch.ingredients[2])
+
+
 // Write a function that takes the lunch object as an argument and returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+const makeSandwich = () => {
+    let ingredient1 = lunch.ingredients[0]
+    let ingredient2 = lunch.ingredients[1]
+    let ingredient3 = lunch.ingredients[2]
+    return `the ingredients for a PB and Banana ${lunch.type} are ${ingredient1}, ${ingredient2}, and ${ingredient3}`
+}
+console.log(makeSandwich(lunch))
+
+
+
 // Update the lunch object with method that returns "The ingredients for a PB and Banana sandwich are bread, peanut butter, and banana."
+console.log(lunch.)
+
+
+
+
+
+
 // Consider this variable:
 
-// const animals = [
-//   { name: "Waffles", type: "dog", age: 7 },
-//   { name: "Fluffy", type: "cat", age: 14 },
-//   { name: "Spelunky", type: "dog", age: 4 },
-//   { name: "Hank", type: "cat", age: 11 }
-// ]
+const animals = [
+  { name: "Waffles", type: "dog", age: 7 },
+  { name: "Fluffy", type: "cat", age: 14 },
+  { name: "Spelunky", type: "dog", age: 4 },
+  { name: "Hank", type: "cat", age: 11 }
+]
 // Create a function that takes in an array of animal objects and returns a new array with only the objects that are cats.
+
 // Create a function that takes in an array of animal objects and returns a new array with only the names of the animals.
 // Create a function that takes in an array of animal objects and returns a new array of the names of the animals that are more than 10 years old.
 // Create a function that takes in an array of animal objects and returns a new array with a sentence about each animal.
